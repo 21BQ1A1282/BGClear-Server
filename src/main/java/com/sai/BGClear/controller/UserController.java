6cd69186e404f092f3a951cf5b199e80d3e97fb4
Sai Manikanta Madhavarapu <madhavarapu.saimanikanta@gmail.com>
2025-09-13 18:34:39 +0530

    file updated

--- src/main/java/com/sai/BGClear/controller/UserController.java
+++ src/main/java/com/sai/BGClear/controller/UserController.java
@@ -7,10 +7,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/users")
@@ -36,6 +36,38 @@ public class UserController {
             ClearBgResponse response = ClearBgResponse.builder()
                     .success(true)
                     .data(user)
+                    .statusCode(HttpStatus.OK)
+                    .build();
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        }catch (Exception exception){
+            ClearBgResponse response = ClearBgResponse.builder()
+                    .success(false)
+                    .data(exception.getMessage())
+                    .statusCode(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .build();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        }
+    }
+
+    @GetMapping("/credits")
+    public ResponseEntity<?> getUserCredits(Authentication authentication) {
+        try{
+            if(authentication.getName().isEmpty() || authentication.getName() == null){
+                ClearBgResponse response = ClearBgResponse.builder()
+                        .success(false)
+                        .data("Unauthorized: Clerk ID missing")
+                        .statusCode(HttpStatus.FORBIDDEN)
+                        .build();
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).body(response);
+            }
+
+            String clerkId = authentication.getName();
+            UserDTO existingUser = userService.getUserByClerkId(clerkId);
+            Map<String, Integer> map = new HashMap<>();
+            map.put("credits", existingUser.getCredits());
+            ClearBgResponse response = ClearBgResponse.builder()
+                    .success(true)
+                    .data(map)
                     .statusCode(HttpStatus.OK)
                     .build();
             return ResponseEntity.status(HttpStatus.OK).body(response);
